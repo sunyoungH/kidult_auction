cd228d9d7205f6405419c4abafe681841454893d
user <user@user-PC>
2017-12-14 15:00:30 +0900

    민희 담당 - 내페이지 수정

--- kidult_auction/src/kr/co/kidultAuction/controller/MyPageFrmEvt.java
+++ kidult_auction/src/kr/co/kidultAuction/controller/MyPageFrmEvt.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -13,52 +16,59 @@ import kr.co.kidultAuction.view.MyAuctionFrm;
 import kr.co.kidultAuction.view.MyPageFrm;
 import kr.co.kidultAuction.view.UserEditFrm;
 
-public class MyPageFrmEvt implements ActionListener{
+public class MyPageFrmEvt implements ActionListener {
 	private MyPageFrm mpf;
 	private AuctionMainFrm amf;
-	
-	
+
 	public MyPageFrmEvt(MyPageFrm mpf) {
-		this.mpf=mpf;
-	}//MyPageFrmEvt
-	
+		this.mpf = mpf;
+	}// MyPageFrmEvt
 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
-		if(ae.getSource()==mpf.getBtnMyAuction()) {
+		if (ae.getSource() == mpf.getBtnMyAuction()) {
 			new MyAuctionFrm(amf);
-		}//end if
-		
-		if(ae.getSource() == mpf.getBtnEdit()){
-			 passMsg();
-			 //////사용자 아이디 여기저기서 받아오는 방법////
-			 ///System.out.println("myPage에서 받아오는 관리자 아이디: "+AuctionMainFrm.User_id); 된다!!!!!!!!!! 
-			 ///////////////////////////
-		}//end if
-		
-	}//actonPerformed
+		} // end if
+
+		if (ae.getSource() == mpf.getBtnEdit()) {
+			passMsg();
+		} // end if
+
+	}// actonPerformed
 
 	JButton btn1;
+
 	public void passMsg() {
-		JPanel p=new JPanel();
-		JPasswordField jp=new JPasswordField(10);
-		btn1=new JButton("입력");
+		
+		JFrame p = new JFrame("비밀번호 입력");
+		JPasswordField jp = new JPasswordField(10);
+		JLabel lb=new JLabel("비밀번호를 입력하세요");
+		btn1 = new JButton("입력");
+
+		p.setLayout(null);
+		p.setBounds(100, 100, 255, 200);
+		lb.setBounds(56, 30, 180, 30);
+		jp.setBounds(50, 70, 150, 30);
+		btn1.setBounds(90, 115, 60, 30);
 		p.add(jp);
 		p.add(btn1);
-		
+		p.add(lb);
+		p.setVisible(true);
+
 		btn1.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String passConfirm=String.valueOf(jp.getPassword());
-					if(passConfirm.equals("11")) {
-						System.out.println("확인완료");
-						new UserEditFrm(amf);
-					}//end if
-			}//actionPerformed
-		});
-		JOptionPane.showMessageDialog(null,p);
-	}//passMsg
-		
+				String t = String.valueOf(jp.getPassword());
+				if (t.equals("11")) {
+					new UserEditFrm(amf);
+					p.dispose();
+				} else {
+					JOptionPane.showMessageDialog(null, "비밀번호를 확인해 주세요");
+				}
 
-}//class
+			}
+		});
+
+	}// passMsg
+
+}// class
